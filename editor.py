ba97b10d8efcfe9087075528024f03ee4ca43434
Paulo RG Nascimento <paulorgnascimento@outlook.com>
2023-02-15 23:34:20 -0300

    start

--- editor.py
+++ editor.py
@@ -1,35 +1,14 @@
 import json
 
-json_str = '''
-{
-    "glossary": {
-        "title": "example glossary",
-		"GlossDiv": {
-            "title": "S",
-			"GlossList": {
-                "GlossEntry": {
-                    "ID": "SGML",
-					"SortAs": "SGML",
-					"GlossTerm": "Standard Generalized Markup Language",
-					"Acronym": "SGML",
-					"Abbrev": "ISO 8879:1986",
-					"GlossDef": {
-                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
-						"GlossSeeAlso": ["GML", "XML"]
-                    },
-					"GlossSee": "markup"
-                }
-            }
-        }
-    }
-}
-'''
+with open('sample.json', 'r') as file:
+    json_str = file.read()
 
 json_dict = json.loads(json_str)
 
-json_dict["glossary"]["title"] = "Paulo Teste"
-json_dict["glossary"]["GlossDiv"]["new_item"] = "value for new item"
+json_dict["glossary"]["title"] = "Paulo Teste 2"
+json_dict["glossary"]["GlossDiv"]["new_item"] = "value for new item 2"
 
 new_json_str = json.dumps(json_dict, indent=4)
 
-print(new_json_str)
+with open('sample.json', 'w') as file:
+    file.write(new_json_str)
